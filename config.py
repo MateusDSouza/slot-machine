ba0aa10697bdaccc946463687e0f9927fa618fad
Mateus De Souza <desouza.mateus1996@gmail.com>
2024-05-24 21:32:55 +0200

    First functionalities

---
+++ config.py
@@ -0,0 +1,6 @@
+DEFAULT_IMAGE_SIZE = (300,300)
+FPS = 120
+HEIGHT = 1200
+WIDTH = 1600
+BG_IMAGE_PATH = "graphics/sigma2_bg.png"
+SOUND_TRACK_PATH = "audio/Sigma_Male_Playlist.mp3"
---
+++ main.py
@@ -0,0 +1,44 @@
+import pygame
+from machine import Machine
+from config import *
+import sys
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption("Sigma Slot Machine")
+        self.clock = pygame.time.Clock()
+        self.bg_image = pygame.image.load(BG_IMAGE_PATH).convert_alpha()
+        self.machine = Machine()
+        self.delta_time = 0
+
+        main_sound = pygame.mixer.Sound(SOUND_TRACK_PATH)
+        main_sound.play(loops=-1)
+
+    def run(self):
+        self.start_time = pygame.time.get_ticks()
+
+        while True:
+            # Quit operation - winners never quit
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            current_time = pygame.time.get_ticks()
+            self.delta_time = current_time -self.start_time/1000.0
+            self.start_time = current_time
+            self.screen.fill((0,0,0))
+            self.screen.blit(self.bg_image,(0,0))
+            pygame.display.flip()
+            #self.machine.update(self.delta_time)
+            
+            
+
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
